a2a6841d5a8efcd289e0591db481bdd6c5d1b016
Constanza Pérez <constanza.vp.o@gmail.com>
2024-07-23 17:53:54 -0400

    Formato NavBar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
