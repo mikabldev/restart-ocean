0ef235041c9ac897ec6634cd9ef13a17d671d04f
camilafolks <camila.windmill@gmail.com>
2024-07-23 10:55:12 -0400

    Modifiqué la estructura para añadir lo que teníamos antes

---
+++ src/components/Colaboradores.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+const Colaboradores = () => {
+
+    <div className="contenidoGeneral">
+        <div className="container">
+            <div className="container-carrossel">
+                <div className="carrossel">
+                    <div className="carrossel-item">
+                        <img src="https://images.ecestaticos.com/T7n1X0x3_hzSw6Yl5OlqGFHEws0=/0x110:2183x1339/1600x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F3d9%2F587%2Ffbf%2F3d9587fbf2f19d48cbfa884b48be05b9.jpg"
+                            alt="Imagen 1" draggable="false"></img>
+                    </div>
+                    <div className="carrossel-item">
+                        <img src="https://postgradoveterinaria.com/wp-content/uploads/animales-marinos.jpg"
+                            alt="Imagen 2" draggable="false"></img>
+                    </div>
+                    <div className="carrossel-item">
+                        <img src="https://nubika.es/wp-content/uploads/2021/05/animales-marinos.jpg" alt="Imagen 3"
+                            draggable="false"></img>
+                    </div>
+                    <div className="carrossel-item">
+                        <img src="https://cdn0.bioenciclopedia.com/es/posts/4/8/3/tortuga_marina_384_600.jpg"
+                            alt="Imagen 4" draggable="false"></img>
+                    </div>
+                    <div className="carrossel-item">
+                        <img src="https://mx.oceana.org/wp-content/uploads/sites/17/istock_000017453850_large_1.jpg"
+                            alt="Imagen 5" draggable="false"></img>
+                    </div>
+                    <div className="carrossel-item">
+                        <img src="https://dam.ngenespanol.com/wp-content/uploads/2021/10/pulpo-caracteristicas-que-come-cuantos-cerebros-tiene.jpg"
+                            alt="Imagen 6" draggable="false"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Colaboradores;
