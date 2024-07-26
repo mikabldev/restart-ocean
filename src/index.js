d7e30c2cef2d8fc35bb55ebad528dc1801f9b649
FelipeMolF <felmolinafuentes@gmail.com>
2024-07-26 11:56:05 -0400

    prueba

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
+
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
