2923bfa6525a12b399c611694666fd4918ffb6ac
camilafolks <camila.windmill@gmail.com>
2024-07-31 17:23:03 -0400

    vista Inicio

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Calendar1 from './components/Calendar1';
 import Foro from './Foro';
 import Donaciones from './Donaciones';
-import CardsQS from './components/CardsQS';
-import Carousel from './components/Carousel';
+import Inicio from './Inicio';
 
 function App() {
     return (
         <>
                 <Routes>
-                    <Route path="/calendario" element={<Calendar1 />} />
+                    <Route path="/" element={<Inicio />} />
                     <Route path="/foro" element={<Foro />} />
                     <Route path="/donaciones" element={<Donaciones />} />
-                    <Route path="/quienes-somos" element={<CardsQS />} />
-                    <Route path="/colaboradores" element={<Carousel />} />
                 </Routes>
         </>
     );
--- src/Inicio.js
+++ src/Inicio.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import BackgroundVideo from './components/BackgroundVideo';
+import CardsQS from './components/CardsQS';
+import Carousel from './components/Carousel'
 
 const Inicio = () => {
     return (
-        <div>Inicio</div>
-    )
-}
+        <div>
+            <BackgroundVideo />
+            <h1>Bienvenidos a Restart Ocean</h1>
+            <p>Descripción de la misión y visión del proyecto.</p>
+            <CardsQS />
+            <Carousel/>
+        </div>
+    );
+};
 
 export default Inicio;
