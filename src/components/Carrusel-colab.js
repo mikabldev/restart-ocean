c4338d9dc2155e3798cf28bd3f8a2c3fce734837
FelipeMolF <felmolinafuentes@gmail.com>
2024-07-31 14:03:52 -0400

    Cambie la fuente de las imagenes a las que hay en la carpeta img del proyecto, para no usar links de intertet y tener todo un poco mas ordenado

--- src/components/Carrusel-colab.js
+++ src/components/Carrusel-colab.js
@@ -6,35 +6,35 @@ export default function Carousel() {
   return (
     <MDBCarousel showControls showIndicators>
       <MDBCarouselItem itemId={1}>
-        <img src='https://www.ultraimagehub.com/wallpapers/tr:flp-false,gx-0.6,gy-0.1,q-75,rh-3264,rw-5824,th-1080,tw-1920/1236878027892461668.jpeg' className='d-block w-100 carousel-image' alt='...' />
+        <img src={`${process.env.PUBLIC_URL}/img/13.jpg`} className='d-block w-100 carousel-image' alt='...' />
         <MDBCarouselCaption>
           <h5>Colaborador 1</h5>
           <p>Descripción  de los colaboradores</p>
         </MDBCarouselCaption>
       </MDBCarouselItem>
       <MDBCarouselItem itemId={2}>
-        <img src='https://www.10wallpaper.com/wallpaper/2560x1600/1603/Dolphin_underwater-Marine_life_HD_Wallpaper_2560x1600.jpg' className='d-block w-100 carousel-image' alt='...' />
+        <img src={`${process.env.PUBLIC_URL}/img/2.jpg`} className='d-block w-100 carousel-image' alt='...' />
         <MDBCarouselCaption>
           <h5>Colaborador 2</h5>
           <p>Descripción  de los colaboradores</p>
         </MDBCarouselCaption>
       </MDBCarouselItem>
       <MDBCarouselItem itemId={3}>
-        <img src='https://static.nationalgeographic.es/files/styles/image_3200/public/01_oceancreatures_mjed49.jpg?w=1600&h=900' className='d-block w-100 carousel-image' alt='...' />
+        <img src={`${process.env.PUBLIC_URL}/img/3.jpg`} className='d-block w-100 carousel-image' alt='...' />
         <MDBCarouselCaption>
           <h5>Colaborador 3</h5>
           <p>Descripción de los colaboradores</p>
         </MDBCarouselCaption>
       </MDBCarouselItem>
       <MDBCarouselItem itemId={4}>
-        <img src='https://www.example.com/image4.jpg' className='d-block w-100 carousel-image' alt='...' />
+        <img src={`${process.env.PUBLIC_URL}/img/4.jpg`} className='d-block w-100 carousel-image' alt='...' />
         <MDBCarouselCaption>
           <h5>Colaborador 4</h5>
           <p>Descripción de los colaboradores</p>
         </MDBCarouselCaption>
       </MDBCarouselItem>
       <MDBCarouselItem itemId={5}>
-        <img src='https://www.example.com/image5.jpg' className='d-block w-100 carousel-image' alt='...' />
+        <img src={`${process.env.PUBLIC_URL}/img/6.jpg`} className='d-block w-100 carousel-image' alt='...' />
         <MDBCarouselCaption>
           <h5>Colaborador 5</h5>
           <p>Descripción de los colaboradores</p>
