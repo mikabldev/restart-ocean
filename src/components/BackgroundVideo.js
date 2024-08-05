642120a1b638bc57f929853d84f3fd9fbc11439f
EvelynFAV <eveabarca.23@gmail.com>
2024-08-05 15:32:35 -0400

    Agregué cambios del BackgroundVideo mandados por Coty

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -7,11 +7,10 @@ const BackgroundVideo = () => {
     <div className="video-container">
       <video autoPlay loop muted>
         <source src="/videos/background.mp4" type="video/mp4" />
-        Tu navegador no soporta la etiqueta de video.
+        No se puede mostrar este video en tu navegador
       </video>
 
       <div className="content d-flex flex-column ">
-        {/* Aquí puedes agregar el contenido que quieres mostrar sobre el video */}
         <div className=''>
           <h1 className='titulo'>Restart Ocean</h1>
           <p>Reseteando la vida marina</p>
