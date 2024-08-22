d67af56fab243791cc5d48ef2d7d210238ffc2d6
camilafolks <camila.windmill@gmail.com>
2024-08-22 16:34:00 -0400

    modificando LSidebar

--- Frontend/src/components/Foro/Left-Sidebar/LSidebar.js
+++ Frontend/src/components/Foro/Left-Sidebar/LSidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import "LSidebar.module.css"
 
 const LSidebar = () => {
   const [openSections, setOpenSections] = useState({
