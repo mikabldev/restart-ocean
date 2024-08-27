f9f3f3eacd6c7fa4c82148fee71326751faced6e
EvelynFAV <eveabarca.23@gmail.com>
2024-08-27 18:19:17 -0400

    recen

--- Backend/scr/server/app.js
+++ Backend/scr/server/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import { jwtSign, jwtDecode } from '../utils/jwt/jwt.js'
-import { verificarCredenciales, registrarUsuario, getUser, isAdmin } from '../models/models.users.js'
+import { verificarCredenciales, registrarUsuario, getUser } from '../models/models.users.js'
 import { authToken } from '../middlewares/authToken.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -38,10 +38,10 @@ app.post('/login', async (req, res) => {
 
 app.post('/users', async (req, res) => {
   try {
-    const { nombre, apellido, email, password, admin } = req.body
+    const { nombre, apellido, email, password} = req.body
     console.log(nombre, apellido, email, password)
     // obtengo los datos del formulario desde el body
-    await registrarUsuario({ nombre, apellido, email, password, admin })
+    await registrarUsuario({ nombre, apellido, email, password })
     res.status(201).json({ status: true, message: 'Usuario registrado con éxito' })
   } catch (error) {
     res.status(error.code || 500).json({ message: 'Error en la conexión', error })
--- Backend/scr/utils/jwt/jwt.js
+++ Backend/scr/utils/jwt/jwt.js
@@ -2,8 +2,7 @@ import jwt from 'jsonwebtoken'
 
 const JWT_KEY = process.env.JWT_KEY
 
-export const jwtSign = (userId, admin) => {
-  const payload = { userId, admin }; // Iagregué userId y admin en el payload
+export const jwtSign = (payload) => {
   console.log(payload, 'desde jwtSign');
   return jwt.sign(payload, JWT_KEY, { expiresIn: '1h' });
 };
